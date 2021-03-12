09e5515c36ee39dd95b7546e5385a292275b4423
Vinoth Kumar <vinoth@stinsoft.com>
2021-03-12 19:08:05 +0530

    Added JS bundle release and updates check

--- App.js
+++ App.js
@@ -1,23 +1,62 @@
-import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, SafeAreaView, Alert } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { StatusBar } from 'expo-status-bar';
-import { WEB_URL } from "@env"
+import { WEB_URL } from "@env";
+import { Colors } from './app/helpers/Colors';
+import * as Updates from 'expo-updates';
 
 export default function App() {
   const webUrl = WEB_URL !== undefined ? WEB_URL : "https://centrum.b4a.app"
+
+  useEffect(() =>{
+    isUpdateAvailable();
+  })
+
+    async function isUpdateAvailable() {
+      const updateAvailable = await Updates.checkForUpdateAsync();
+      console.log(updateAvailable.isAvailable);
+      if (updateAvailable.isAvailable) {
+        Alert.alert(
+          "Update Available",
+          "An update is available.",
+          [{
+            text: "OK",
+            onPress: () => {
+              async () => {
+                await Updates.fetchUpdateAsync();
+                Updates.reloadFromCache();
+              }
+            }
+          }], {
+            cancelable: false
+          }
+        );
+      }
+    }
+  
+
   return (
-    <WebView source={{ uri: webUrl }} style={{ marginTop: 20 }} >
-      <StatusBar backgroundColor="#663399" style="light" />
-    </WebView>
+    <>
+    <SafeAreaView style={styles.safeViewBG}></SafeAreaView>
+      <StatusBar backgroundColor={Colors.Primary} style="light" />
+      <WebView source={{ uri: webUrl }} 
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        style={styles.container} >
+      </WebView>
+    </>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',s
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  safeViewBG:{
+    backgroundColor: Colors.Primary
+  },
+  container: {
+    flex: 1,
+    backgroundColor: Colors.White,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ app/helpers/Colors.js
@@ -0,0 +1,5 @@
+export const Colors = {
+    White: '#ffffff',
+    Primary: '#663399',
+    Secondary: '#ff8c00'
+}
